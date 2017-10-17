62c742ab44662f068cc762e09f930741dca838e7
Piotr Kulpa <p.kulpa@onet.eu>
2017-10-17 15:25:37 +0200

    image sorting

--- js/images.js
+++ js/images.js
@@ -1,17 +1,46 @@
 (function ($) {
 
-	function Images (src) {
+
+function Images (src) {
 		this.src = src;
 		};
 
 		Images.prototype.doAjax = function () {
-			$.getJSON(this.src, this.loadData)
+			var that = this;
+			return $.getJSON(this.src, this.loadData).done(function (data) {
+				//tutaj dodaj uchwyty sortujace
+
+
+				$("#type").change(function(){
+				  var id = $(this).find("option:selected").attr("id");
+
+				  switch (id){
+				    case "asc-name":
+				      that.sortAscName(data);
+				      break;
+						case "dsc-name":
+				      that.sortDscName(data);
+				      break;
+						case "asc-date":
+				      that.sortAscDate(data);
+				      break;
+						case "dsc-date":
+				      that.sortDscDate(data);
+				      break;
+				  }
+				});
+
+			});
 		};
+
+
 
 		Images.prototype.loadData = function (data) {
 			var html = '',
 			img = [],
 			imgCounter = data.length;
+
+
 
 			$.each(data, function(i,c){
 				html += '<a class="example-image-link" href="images/original/' + c.name + '" data-lightbox="example-2" data-title="Optional caption.">';
@@ -29,9 +58,30 @@
 				}
 				img[i].src = 'images/thumbs/' + c.name;
 				});
+
+		};
+
+		Images.prototype.sortAscName = function (data) {
+			console.log('sortAscName');
+		};
+
+		Images.prototype.sortDscName = function (data) {
+			console.log('sortDscName');
+		};
+
+		Images.prototype.sortAscDate = function (data) {
+			console.log('sortAscDate');
+		};
+
+		Images.prototype.sortDscDate = function (data) {
+			console.log('sortDscDate');
 		};
 
 	var loadImages = new Images("data-img.json");
 	loadImages.doAjax();
+
+
+	// wazne! referencje wykonujemy wewnatrz medoty ktora potrzebuje dostepu do zmiennej prywatnej
+
 
 }) ( jQuery );
